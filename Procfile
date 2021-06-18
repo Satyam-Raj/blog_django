0e742367415c15e09050c9e71bbe9898d3ce8a40
Satyam Raj <44699922+Satyam-Raj@users.noreply.github.com>
2021-06-19 02:24:59 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yup.wsgi --log-file -
---
+++ yup/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yup project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yup.settings')
+
+application = get_asgi_application()
